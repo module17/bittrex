e24f0a95bc67f88fe2f726d4e0a7d7a3b249fed2
Darek Kowalski <module17@gmail.com>
2017-12-24 21:14:14 -0500

    Minor fix to catch Exception in example.php

--- example.php
+++ example.php
@@ -1,13 +1,16 @@
 <?php
 
-	require __DIR__.'/src/edsonmedina/bittrex/Client.php';
+require __DIR__ . '/src/edsonmedina/bittrex/Client.php';
 
-	use edsonmedina\bittrex\Client;
+use edsonmedina\bittrex\Client;
 
-	$key = '4bec433f95e54562aeeefae92ebedb84';
-	$secret = '6171690af7364ea2a951dc85d00e1130';
+$key = 'ffffff';
+$secret = '000000';
 
-	$b = new Client ($key, $secret);
-	var_dump ($b->getOrderHistory ());
-	
-	echo "\n\n";
+try {
+    $b = new Client($key, $secret);
+    var_dump($b->getOrderHistory());
+    echo PHP_EOL . PHP_EOL;
+} catch (Exception $e) {
+    echo sprintf('ERROR: %s' . PHP_EOL . PHP_EOL, $e->getMessage());
+}
